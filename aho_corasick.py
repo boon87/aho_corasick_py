5db15faeaf4abedd477bb9e2ee515a73ff9deb72
Boon Wenjie <boonwenjie@gmail.com>
2015-07-02 00:10:43 +0800

    Construction of goto function
    Ahocorasick to have all its methods implemented within it's own class.
    Implemented algorithm 2 construction of goto function

---
+++ aho_corasick.py
@@ -0,0 +1,66 @@
+__author__ = 'boon'
+
+from collections import deque
+from collections import defaultdict
+
+class AhoCorasick():
+    def __init__(self):
+        self.output = defaultdict(set)
+        self.goto = defaultdict(dict)
+        self.failure = {}
+
+    def initialize(self, keywords):
+        newstate = 0
+        for keyword in keywords:
+            newstate = self.construct_goto(keyword, newstate)
+
+    def construct_goto(self, keyword, newstate):
+        state = 0
+        for char in keyword:
+            if self.get_goto_state_for_build(state, char) is not None:
+                state = self.get_goto_state_for_build(state, char)
+                continue
+            else:
+                newstate += 1
+                self.goto[state][char] = newstate
+                state = newstate
+
+        self.output[state].add(keyword)
+
+        return newstate
+
+    def get_goto_state_for_build(self, state, char):
+        """
+        Used during the building of state. Does not loop back state 0
+        :param state:
+        :param char:
+        :return:
+        """
+        if char not in self.goto.get(state, {}):
+            return None
+        else:
+            return self.goto[state][char]
+
+    def get_goto_state(self, state, char):
+        """
+        Used during normal search_operation. Loops back to state 0
+        :param state:
+        :param char:
+        :return:
+        """
+        if char not in self.goto.get(state, {}):
+            return 0 if state == 0 else None
+        else:
+            return self.goto[state][char]
+
+
+def main():
+    ac = AhoCorasick()
+    ac.initialize(["she", "he", "hers", "his"])
+
+    print("Testing")
+    print("Contents in goto:", ac.goto)
+    print("Contents in output:", ac.output)
+
+if __name__ == '__main__':
+    main()
